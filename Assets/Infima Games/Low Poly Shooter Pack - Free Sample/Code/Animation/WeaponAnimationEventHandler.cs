b01e3e8b6f85ba93f28d3b069fdb640c1c538fc9
Viviiii <viclabeyrie@outlook.com>
2022-12-08 21:26:22 +0100

    Meta file issue

--- Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Animation/WeaponAnimationEventHandler.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7866b22f86bf7abcc9a86aae394de62faeb34abeeaba621e7671e68fc4c89627
-size 1027
--- Assets/Scripts/Enemy.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:be5b9a1aea7ba0497176c1d0b4fad3b3e9343b862969a70f7ed750efa51c5115
-size 450
